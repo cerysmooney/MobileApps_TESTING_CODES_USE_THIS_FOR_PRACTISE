d0b2c9b9d8e8d0562e4587416bdc41824c95db91
cerys mooney <Cerys2.mooney@live.uwe.ac.uk>
2023-11-09 19:07:10 +0000

    Initial commit

---
+++ app/src/androidTest/java/com/example/mobileapps_testing_codes_use_this_for_practise/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package com.example.mobileapps_testing_codes_use_this_for_practise
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals(
+            "com.example.mobileapps_testing_codes_use_this_for_practise",
+            appContext.packageName
+        )
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISE"
+include(":app")
+ 
