59acb76bc7886b9aabbc1f2e96dbb10d4e474348
LauraaMae <laura2.robbie@live.uwe.ac.uk>
2023-11-10 14:14:28 +0000

    Calendar creation

---
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/CalendarPage.kt
@@ -0,0 +1,63 @@
+package com.example.mobileapps_testing_codes_use_this_for_practise
+
+import android.os.Bundle
+import android.widget.CalendarView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobileapps_testing_codes_use_this_for_practise.ui.theme.MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme
+
+class CalendarPage : ComponentActivity() {
+    lateinit var dateTV: TextView
+    lateinit var calendarView: CalendarView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calendar_page)
+
+        // initializing variables of
+        // list view with their ids.
+        dateTV = findViewById(R.id.idTVDate)
+        calendarView = findViewById(R.id.calendarView)
+
+        // on below line we are adding set on
+        // date change listener for calendar view.
+        calendarView
+            .setOnDateChangeListener(
+                OnDateChangeListener { view, year, month, dayOfMonth ->
+                    // In this Listener we are getting values
+                    // such as year, month and day of month
+                    // on below line we are creating a variable
+                    // in which we are adding all the variables in it.
+                    val Date = (dayOfMonth.toString() + "-"
+                            + (month + 1) + "-" + year)
+
+                    // set this date in TextView for Display
+                    dateTV.setText(Date)
+                })
+
+    }
+}
+
+@Composable
+fun Greeting3(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview3() {
+    MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme {
+        Greeting3("Android")
+    }
+}
