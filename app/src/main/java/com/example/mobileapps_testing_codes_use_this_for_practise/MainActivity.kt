84c90f090b77ef16d381b15c87b6777fe7d1537a
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2023-11-13 17:12:59 +0000

    the seekbar didnt work!!!!!

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.mobileapps_testing_codes_use_this_for_practise
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,18 +23,65 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("peepee")
+        setContentView(R.layout.my_layout)
+
+        val seek = findViewById<SeekBar>(R.id.seekBar2)
+        val awful = findViewById<TextView>(R.id.awful_emotion); findViewById<ImageView>(R.id.awful_owl)
+        val sad = findViewById<TextView>(R.id.sad_emotion); findViewById<ImageView>(R.id.sad_owl)
+        val okay = findViewById<TextView>(R.id.ok_emotion); findViewById<ImageView>(R.id.ok_owl)
+        val happy = findViewById<TextView>(R.id.happy_emotion); findViewById<ImageView>(R.id.happy_owl)
+        val awesome = findViewById<TextView>(R.id.awesome_emotion); findViewById<ImageView>(R.id.awesome_owl)
+        seek.setMax(4)
+        seek.setProgress(2)
+
+        seek?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean){
+                if (progress == 0){
+                    awful.setVisibility(View.VISIBLE)
+                    sad.setVisibility(View.INVISIBLE)
+                    okay.setVisibility(View.INVISIBLE)
+                    happy.setVisibility(View.INVISIBLE)
+                    awesome.setVisibility(View.INVISIBLE)
+                }
+                else if (progress == 1){
+                    awful.setVisibility(View.INVISIBLE)
+                    sad.setVisibility(View.VISIBLE)
+                    okay.setVisibility(View.INVISIBLE)
+                    happy.setVisibility(View.INVISIBLE)
+                    awesome.setVisibility(View.INVISIBLE)
+                }
+                else if (progress == 2){
+                    awful.setVisibility(View.INVISIBLE)
+                    sad.setVisibility(View.INVISIBLE)
+                    okay.setVisibility(View.VISIBLE)
+                    happy.setVisibility(View.INVISIBLE)
+                    awesome.setVisibility(View.INVISIBLE)
+                }
+                else if (progress == 3){
+                    awful.setVisibility(View.INVISIBLE)
+                    sad.setVisibility(View.INVISIBLE)
+                    okay.setVisibility(View.INVISIBLE)
+                    happy.setVisibility(View.VISIBLE)
+                    awesome.setVisibility(View.INVISIBLE)
+                }
+                else if (progress == 4){
+                    awful.setVisibility(View.INVISIBLE)
+                    sad.setVisibility(View.INVISIBLE)
+                    okay.setVisibility(View.INVISIBLE)
+                    happy.setVisibility(View.INVISIBLE)
+                    awesome.setVisibility(View.VISIBLE)
                 }
             }
-        }
-        setContentView(R.layout.my_layout)
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+        })
+
         val namePageButton = findViewById<Button>(R.id.button)
         namePageButton.setOnClickListener{
             val Intent = Intent(this,NamePage::class.java)
