730eb16dbef2853e2643ceafc71792dfed60c424
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2023-11-13 17:46:17 +0000

    the seekbar didnt work!!!!!

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import com.example.mobileapps_testing_codes_use_this_for_practise.ui.theme.MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme
 
 class MainActivity : ComponentActivity() {
@@ -26,59 +28,55 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.my_layout)
 
         val seek = findViewById<SeekBar>(R.id.seekBar2)
-        val awful = findViewById<TextView>(R.id.awful_emotion); findViewById<ImageView>(R.id.awful_owl)
-        val sad = findViewById<TextView>(R.id.sad_emotion); findViewById<ImageView>(R.id.sad_owl)
-        val okay = findViewById<TextView>(R.id.ok_emotion); findViewById<ImageView>(R.id.ok_owl)
-        val happy = findViewById<TextView>(R.id.happy_emotion); findViewById<ImageView>(R.id.happy_owl)
-        val awesome = findViewById<TextView>(R.id.awesome_emotion); findViewById<ImageView>(R.id.awesome_owl)
-        seek.setMax(4)
-        seek.setProgress(2)
+        val awful = findViewById<TextView>(R.id.awful_emotion)
+        val sad = findViewById<TextView>(R.id.sad_emotion)
+        val okay = findViewById<TextView>(R.id.ok_emotion)
+        val happy = findViewById<TextView>(R.id.happy_emotion)
+        val awesome = findViewById<TextView>(R.id.awesome_emotion)
 
         seek?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean){
                 if (progress == 0){
-                    awful.setVisibility(View.VISIBLE)
-                    sad.setVisibility(View.INVISIBLE)
-                    okay.setVisibility(View.INVISIBLE)
-                    happy.setVisibility(View.INVISIBLE)
-                    awesome.setVisibility(View.INVISIBLE)
+                    awful.isVisible
+                    sad.isInvisible
+                    okay.isInvisible
+                    happy.isInvisible
+                    awesome.isInvisible
                 }
                 else if (progress == 1){
-                    awful.setVisibility(View.INVISIBLE)
-                    sad.setVisibility(View.VISIBLE)
-                    okay.setVisibility(View.INVISIBLE)
-                    happy.setVisibility(View.INVISIBLE)
-                    awesome.setVisibility(View.INVISIBLE)
+                    awful.isInvisible
+                    sad.isVisible
+                    okay.isInvisible
+                    happy.isInvisible
+                    awesome.isInvisible
                 }
                 else if (progress == 2){
-                    awful.setVisibility(View.INVISIBLE)
-                    sad.setVisibility(View.INVISIBLE)
-                    okay.setVisibility(View.VISIBLE)
-                    happy.setVisibility(View.INVISIBLE)
-                    awesome.setVisibility(View.INVISIBLE)
+                    awful.isInvisible
+                    sad.isInvisible
+                    okay.isVisible
+                    happy.isInvisible
+                    awesome.isInvisible
                 }
                 else if (progress == 3){
-                    awful.setVisibility(View.INVISIBLE)
-                    sad.setVisibility(View.INVISIBLE)
-                    okay.setVisibility(View.INVISIBLE)
-                    happy.setVisibility(View.VISIBLE)
-                    awesome.setVisibility(View.INVISIBLE)
+                    awful.isInvisible
+                    sad.isVisible
+                    okay.isInvisible
+                    happy.isVisible
+                    awesome.isInvisible
                 }
                 else if (progress == 4){
-                    awful.setVisibility(View.INVISIBLE)
-                    sad.setVisibility(View.INVISIBLE)
-                    okay.setVisibility(View.INVISIBLE)
-                    happy.setVisibility(View.INVISIBLE)
-                    awesome.setVisibility(View.VISIBLE)
+                    awful.isInvisible
+                    sad.isInvisible
+                    okay.isInvisible
+                    happy.isInvisible
+                    awesome.isVisible
                 }
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                TODO("Not yet implemented")
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                TODO("Not yet implemented")
             }
         })
 
