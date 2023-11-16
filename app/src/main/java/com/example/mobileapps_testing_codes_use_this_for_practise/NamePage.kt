73f60d1e823b3213d54d7530ea343adba28965fe
cerys mooney <Cerys2.mooney@live.uwe.ac.uk>
2023-11-16 18:52:33 +0000

    made name page and xml

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/NamePage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/NamePage.kt
@@ -1,9 +1,6 @@
 package com.example.mobileapps_testing_codes_use_this_for_practise
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,37 +14,7 @@ import com.example.mobileapps_testing_codes_use_this_for_practise.ui.theme.Mobil
 
 class NamePage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.name_page)
-//        val owl = findViewById<ImageView>(R.id.nameOwl)
-//        val button = findViewById<Button>(R.id.continueButton)
-
-//
-
-
-
-////
-//        val calendarPageButton = findViewById<Button>(R.id.button3)
-//        calendarPageButton.setOnClickListener{
-//            val Intent = Intent(this, CalendarPage::class.java)
-//            startActivity(Intent)
-        }
-    }
-
-
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme {
-        Greeting2("Android")
     }
 }
