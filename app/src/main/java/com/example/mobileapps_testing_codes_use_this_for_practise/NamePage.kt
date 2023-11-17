2890ef19d124034e968cef96f12ea4fe49c92712
cerys mooney <Cerys2.mooney@live.uwe.ac.uk>
2023-11-17 15:57:41 +0000

    Cleanup namepage

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/NamePage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/NamePage.kt
@@ -1,24 +1,13 @@
 package com.example.mobileapps_testing_codes_use_this_for_practise
 
-import android.content.ContentValues.TAG
+
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.mobileapps_testing_codes_use_this_for_practise.ui.theme.MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme
-import com.google.android.material.textfield.TextInputEditText
 class NamePage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +30,7 @@ class NamePage : ComponentActivity() {
                 it.putExtra("textSave", name)
                 startActivity(it)
             }
-//            startActivity(Intent)
+
         }
     }
 }
