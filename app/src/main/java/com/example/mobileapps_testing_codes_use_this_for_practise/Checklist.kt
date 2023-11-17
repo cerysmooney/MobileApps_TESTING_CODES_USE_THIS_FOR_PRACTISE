b2410b28a0875363d9ae963b79a779b699b39103
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2023-11-17 10:13:52 +0000

    creating checklist page

---
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/Checklist.kt
@@ -0,0 +1,19 @@
+package com.example.mobileapps_testing_codes_use_this_for_practise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobileapps_testing_codes_use_this_for_practise.ui.theme.MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme
+
+class Checklist : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
