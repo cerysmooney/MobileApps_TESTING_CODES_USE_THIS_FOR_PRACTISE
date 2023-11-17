4df245a17b799847ce78a616fb4b55eb985138b7
cerys mooney <Cerys2.mooney@live.uwe.ac.uk>
2023-11-17 17:12:24 +0000

    added feelings page

---
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/ExpandOnFeelingsPage.kt
@@ -0,0 +1,20 @@
+package com.example.mobileapps_testing_codes_use_this_for_practise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobileapps_testing_codes_use_this_for_practise.ui.theme.MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme
+
+class ExpandOnFeelingsPage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.expand_on_feelings_page)
+    }
+}
