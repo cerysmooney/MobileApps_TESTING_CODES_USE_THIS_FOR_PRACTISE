761fbfeacb96bc9b4120c414d604e75aae870f51
LauraaMae <laura2.robbie@live.uwe.ac.uk>
2023-11-21 18:47:40 +0000

    crying

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/HomePage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/HomePage.kt
@@ -1,10 +1,12 @@
 package com.example.mobileapps_testing_codes_use_this_for_practise
 
 import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,19 +21,29 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.mobileapps_testing_codes_use_this_for_practise.ui.theme.MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme
 
 class HomePage : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page)
 
-//        val bgcolorsaved = intent.getStringExtra("saveBG")
-//        val checkcolor = findViewById<ConstraintLayout>(R.id.homePageLayout)
-//
-//        checkcolor.setBackgroundColor(Color.parseColor(bgcolorsaved))
 
-      val name = intent.getStringExtra("textSave")
-        val introduction = findViewById<TextView>(R.id.intro).apply{
-            text = "Hello" +  " " + name
+
+
+
+
+
+
+        val name = intent.getStringExtra("textSave")
+        val introduction = findViewById<TextView>(R.id.intro).apply {
+            text = "Hello" + " " + name
         }
 
-    }
+
+
+        }
 }
+
+
+
+
