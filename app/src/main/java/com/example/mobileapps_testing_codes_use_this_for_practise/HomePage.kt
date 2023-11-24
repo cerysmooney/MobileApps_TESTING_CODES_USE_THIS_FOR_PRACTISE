e8d72df478653891bef852b2cce60960fd3edec4
LauraaMae <laura2.robbie@live.uwe.ac.uk>
2023-11-24 09:24:39 +0000

    Its Ugly but it works

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/HomePage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/HomePage.kt
@@ -33,11 +33,21 @@ class HomePage : ComponentActivity() {
         //Keeping the page background the same
         val imageChange = findViewById<ConstraintLayout>(R.id.homePageLayout)
 
+
+        //Button for Expanding on Feelings
+        val expandOnFeelingsPageButton = findViewById<ImageButton>(R.id.imagePlusIcon)
+        expandOnFeelingsPageButton.setOnClickListener{
+            val Intent = Intent(this,ExpandOnFeelingsPage::class.java)
+            startActivity(Intent)
+        }
+
+
+
         //Calling values for the image buttons to allow for changing background
         val imageCross = findViewById<ImageButton>(R.id.imageCross)
         val imageCross2 = findViewById<ImageButton>(R.id.imageCross2)
         val imageCross3 = findViewById<ImageButton>(R.id.imageCross3)
-        val imageCross4 = findViewById<ImageButton>(R.id.imageCross4)
+        val imageCross4 = findViewById<ImageButton>(R.id.goalsPageButton)
 
         //Changing the background on click for each button
         imageCross.setOnClickListener{
@@ -58,7 +68,16 @@ class HomePage : ComponentActivity() {
         val introduction = findViewById<TextView>(R.id.intro).apply {
             text = "Hello" + " " + name
         }
+
+        //Button for getting to goals page
+        val goalsPageButton = findViewById<ImageButton>(R.id.goalsPageButton)
+        goalsPageButton.setOnClickListener{
+            val Intent = Intent(this,GoalsPage::class.java)
+            startActivity(Intent)
         }
+
+
+    }
 }
 
 
