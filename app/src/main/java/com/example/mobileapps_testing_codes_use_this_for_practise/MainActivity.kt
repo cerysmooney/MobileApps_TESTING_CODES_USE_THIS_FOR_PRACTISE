fd73b71d5bfc1489e0ce0b7d57a18f8132b8682e
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2023-11-30 19:42:56 +0000

    universal name value

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         Thread.sleep(1000)
         installSplashScreen()
