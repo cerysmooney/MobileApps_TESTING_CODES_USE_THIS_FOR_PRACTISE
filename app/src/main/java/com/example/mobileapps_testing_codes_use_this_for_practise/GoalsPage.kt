321c500823924cf529261133e01ede9458ce6826
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2023-12-07 19:00:21 +0000

    commenting out

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/GoalsPage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/GoalsPage.kt
@@ -7,31 +7,31 @@ import androidx.activity.ComponentActivity
 class GoalsPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.goals_page)
+//        setContentView(R.layout.goals_page)
 
-        //Calling values for the image buttons to allow for changing background
-        val imageCross = findViewById<ImageButton>(R.id.imageCross)
-        val imageCross2 = findViewById<ImageButton>(R.id.imageCross2)
-        val imageCross3 = findViewById<ImageButton>(R.id.imageCross3)
-
-
-        //Changing the background on click for each button
-        imageCross.setOnClickListener{
-            imageCross.setBackgroundResource(R.drawable.check)
-        }
-        imageCross2.setOnClickListener{
-            imageCross2.setBackgroundResource(R.drawable.check)
-        }
-        imageCross3.setOnClickListener{
-            imageCross3.setBackgroundResource(R.drawable.check)
-        }
-
-
-        //Button for getting to goals page
-        val homeGoalsPageButton = findViewById<ImageButton>(R.id.goBack)
-        homeGoalsPageButton.setOnClickListener{
-            val Intent = Intent(this,HomePage::class.java)
-            startActivity(Intent)
-        }
+//        //Calling values for the image buttons to allow for changing background
+//        val imageCross = findViewById<ImageButton>(R.id.imageCross)
+//        val imageCross2 = findViewById<ImageButton>(R.id.imageCross2)
+//        val imageCross3 = findViewById<ImageButton>(R.id.imageCross3)
+//
+//
+//        //Changing the background on click for each button
+//        imageCross.setOnClickListener{
+//            imageCross.setBackgroundResource(R.drawable.check)
+//        }
+//        imageCross2.setOnClickListener{
+//            imageCross2.setBackgroundResource(R.drawable.check)
+//        }
+//        imageCross3.setOnClickListener{
+//            imageCross3.setBackgroundResource(R.drawable.check)
+//        }
+//
+//
+//        //Button for getting to goals page
+//        val homeGoalsPageButton = findViewById<ImageButton>(R.id.goBack)
+//        homeGoalsPageButton.setOnClickListener{
+//            val Intent = Intent(this,HomePage::class.java)
+//            startActivity(Intent)
+//        }
     }
 }
