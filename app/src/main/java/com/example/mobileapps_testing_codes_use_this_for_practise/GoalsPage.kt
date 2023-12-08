7adf444269e2a78a8f9617b94395cda806b74381
LauraaMae <laura2.robbie@live.uwe.ac.uk>
2023-12-08 12:14:56 +0000

    aaggghhhh

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/GoalsPage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/GoalsPage.kt
@@ -1,8 +1,7 @@
 package com.example.mobileapps_testing_codes_use_this_for_practise
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView
+
 import androidx.activity.ComponentActivity
 
 class GoalsPage : ComponentActivity() {
