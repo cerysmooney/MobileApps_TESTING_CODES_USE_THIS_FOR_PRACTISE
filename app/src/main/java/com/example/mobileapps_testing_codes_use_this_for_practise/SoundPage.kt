f8d7e694cadae62d6f757824728b71e76ccb2bf3
LauraaMae <laura2.robbie@live.uwe.ac.uk>
2023-12-08 14:04:29 +0000

    sound again again

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/SoundPage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/SoundPage.kt
@@ -1,7 +1,9 @@
 package com.example.mobileapps_testing_codes_use_this_for_practise
 
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.Window
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.mobileapps_testing_codes_use_this_for_practise.ui.theme.MobileApps_TESTING_CODES_USE_THIS_FOR_PRACTISETheme
 
 class SoundPage : ComponentActivity() {
@@ -21,20 +24,73 @@ class SoundPage : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Transition for getting to the page
+        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+        overrideActivityTransition(
+            OVERRIDE_TRANSITION_OPEN,
+            R.anim.slide_in_right,
+            R.anim.slide_out_left
+        )
+
         setContentView(R.layout.sound_page)
 
-        // Initialize MediaPlayer
+        //BG background
+        val bgcolorsaved = bgValue.bgColourChange
+        val checkcolor = findViewById<ConstraintLayout>(R.id.sound_page_layout)
+        checkcolor.setBackgroundColor(Color.parseColor(bgcolorsaved))
 
-        val playButton = findViewById<ImageButton>(R.id.playSound)
 
-        playButton.setOnClickListener {
+
+        // Initialize MediaPlayer Happy
+        val playButtonHappy = findViewById<ImageButton>(R.id.playSoundHappy)
+
+        playButtonHappy.setOnClickListener {
 
             val mediaPlayer = MediaPlayer.create(this, R.raw.birds)
 
             // Check if the audio is playing
             if (mediaPlayer.isPlaying) {
                 // If playing, pause and update button text
-                mediaPlayer.pause()
+                mediaPlayer.stop()
+//                playButton.text = "Play Audio"
+            } else {
+                // If not playing, start playing and update button text
+                mediaPlayer.start()
+//                playButton.text = "Pause Audio"
+            }
+        }
+
+        // Initialize MediaPlayer Okay
+        val playButtonOkay = findViewById<ImageButton>(R.id.playSoundOkay)
+
+        playButtonOkay.setOnClickListener {
+
+            val mediaPlayer = MediaPlayer.create(this, R.raw.birds)
+
+            // Check if the audio is playing
+            if (mediaPlayer.isPlaying) {
+                // If playing, pause and update button text
+                mediaPlayer.stop()
+//                playButton.text = "Play Audio"
+            } else {
+                // If not playing, start playing and update button text
+                mediaPlayer.start()
+//                playButton.text = "Pause Audio"
+            }
+        }
+
+        // Initialize MediaPlayer Awful
+        val playButtonSad = findViewById<ImageButton>(R.id.playSoundSad)
+
+        playButtonSad.setOnClickListener {
+
+            val mediaPlayer = MediaPlayer.create(this, R.raw.birds)
+
+            // Check if the audio is playing
+            if (mediaPlayer.isPlaying) {
+                // If playing, pause and update button text
+                mediaPlayer.stop()
 //                playButton.text = "Play Audio"
             } else {
                 // If not playing, start playing and update button text
