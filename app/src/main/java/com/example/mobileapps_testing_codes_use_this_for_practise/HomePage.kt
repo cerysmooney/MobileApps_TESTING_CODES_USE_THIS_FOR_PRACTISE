3aad07f833c40d55b8668036a5d7ee8845abc1e2
LauraaMae <laura2.robbie@live.uwe.ac.uk>
2023-12-11 14:42:01 +0000

    commenting

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/HomePage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/HomePage.kt
@@ -19,34 +19,34 @@ class HomePage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//Transition for getting to the page
+        //Transition for getting to the page
         window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
         overrideActivityTransition(
             OVERRIDE_TRANSITION_OPEN,
             R.anim.slide_in_right,
             R.anim.slide_out_left
         )
-//Connecting to Layout
+        //Connecting to Layout
         setContentView(R.layout.home_page)
 
-//BG background
+        //BG background
         val bgcolorsaved = bgValue.bgColourChange
         val checkcolor = findViewById<ConstraintLayout>(R.id.homePageLayout)
         checkcolor.setBackgroundColor(Color.parseColor(bgcolorsaved))
 
-//Getting date and displaying in textview
+        //Getting date and displaying in textview
         val textView: TextView = findViewById(R.id.dateAndTime)
         val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy")
         val currentDateAndTime: String = simpleDateFormat.format(Date())
         textView.text = currentDateAndTime
 
-//Calling values for the image buttons to allow for changing background
+        //Calling values for the image buttons to allow for changing background
         val imageCross = findViewById<ImageButton>(R.id.imageCross)
         val imageCross2 = findViewById<ImageButton>(R.id.imageCross2)
         val imageCross3 = findViewById<ImageButton>(R.id.imageCross3)
         val imageCross4 = findViewById<ImageButton>(R.id.goalsPageButton)
 
-//Changing the background on click for each button
+        //Changing the background on click for each button
         imageCross.setOnClickListener {
             imageCross.setBackgroundResource(R.drawable.check)
         }
@@ -57,14 +57,14 @@ class HomePage : ComponentActivity() {
             imageCross3.setBackgroundResource(R.drawable.check)
         }
 
- // Button for getting goals page
+        // Button for getting goals page
         val goalsPageButton = findViewById<ImageButton>(R.id.goalsPageButton)
         goalsPageButton.setOnClickListener {
             val Intent = Intent(this, SoundPage::class.java)
             startActivity(Intent)
         }
 
-//Displaying hello and the name chosen from the name page
+        //Displaying hello and the name chosen from the name page
         //val name = intent.getStringExtra("textSave")
         //Log.d("HomePage", "name: $name")
         val callName = NameValue.nameData
@@ -75,14 +75,14 @@ class HomePage : ComponentActivity() {
 
         //Log.d("HomePage","name: $callName")
 
-//Button for getting to calendar page
+        //Button for getting to calendar page
         val homeSoundPageButton = findViewById<ImageButton>(R.id.reflectionPageButton)
         homeSoundPageButton.setOnClickListener {
             val Intent = Intent(this, SoundPage::class.java)
             startActivity(Intent)
         }
 
-// Button for getting goals page
+        // Button for getting goals page
         val homeGoalsPageButton = findViewById<ImageButton>(R.id.reflectionPageButton3)
         homeGoalsPageButton.setOnClickListener {
             val Intent = Intent(this,GoalsPage::class.java)
@@ -91,7 +91,7 @@ class HomePage : ComponentActivity() {
         //
 
 
-//Button for Expanding on Feelings
+        //Button for Expanding on Feelings
         val expandOnFeelingsPageButton = findViewById<ImageButton>(R.id.imagePlusIcon)
         expandOnFeelingsPageButton.setOnClickListener {
 //            val fade: Transition = TransitionInflater.from(this).inflateTransition(R.transition.fade_transition)
