dcad78ca95f31a4410e5d72d3d02195838cb9869
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2023-12-12 16:48:22 +0000

    debugging

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/GoalsPage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/GoalsPage.kt
@@ -1,5 +1,46 @@
 package com.example.mobileapps_testing_codes_use_this_for_practise
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mobileapps_testing_codes_use_this_for_practise.databinding.ActivityMainBinding
 
-class GoalsPage : AppCompatActivity(){}
+class GoalsPage : AppCompatActivity(), TaskItemClickListener
+{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
+        }
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView()
+    {
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+            }
+        }
+    }
+
+    override fun editTaskItem(taskItem: TaskItem)
+    {
+        NewTaskSheet(taskItem).show(supportFragmentManager,"newTaskTag")
+    }
+
+    override fun completeTaskItem(taskItem: TaskItem)
+    {
+        taskViewModel.setCompleted(taskItem)
+    }
+}
---
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/TaskItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.example.mobileapps_testing_codes_use_this_for_practise
+
+interface TaskItemClickListener
+{
+    fun editTaskItem(taskItem: TaskItem)
+    fun completeTaskItem(taskItem: TaskItem)
+}
