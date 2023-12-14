1613304820dc51469f8c5dd31c3dc5f9fd38b2f6
RandumbFluke <luke2.hammond@live.uwe.ac.uk>
2023-12-14 16:29:26 +0000

    goals page saves data

--- app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/GoalsPage.kt
+++ app/src/main/java/com/example/mobileapps_testing_codes_use_this_for_practise/GoalsPage.kt
@@ -5,10 +5,34 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
+class GPData1 {
+    companion object {
+        var goalsData1: String = ""
+    }
+}
+
+class GPData2 {
+    companion object {
+        var goalsData2: String = ""
+    }
+}
+
+class GPData3 {
+    companion object {
+        var goalsData3: String = ""
+    }
+}
+
+class GPData4 {
+    companion object {
+        var goalsData4: String = ""
+    }
+}
 class GoalsPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +48,32 @@ class GoalsPage : ComponentActivity() {
 
         //Connecting to Layout
         setContentView(R.layout.goals_page)
+
+        //Goals Text
+        val goal1 = findViewById<EditText>(R.id.typeYourGoal1)
+        val goal2 = findViewById<EditText>(R.id.typeYourGoal2)
+        val goal3 = findViewById<EditText>(R.id.typeYourGoal3)
+        val goal4 = findViewById<EditText>(R.id.typeYourGoal4)
+
+        val setGoal1 = GPData1.goalsData1
+        findViewById<EditText>(R.id.typeYourGoal1).apply {
+            setText(setGoal1)
+        }
+
+        val setGoal2 = GPData2.goalsData2
+        findViewById<EditText>(R.id.typeYourGoal2).apply {
+            setText(setGoal2)
+        }
+
+        val setGoal3 = GPData3.goalsData3
+        findViewById<EditText>(R.id.typeYourGoal3).apply {
+            setText(setGoal3)
+        }
+
+        val setGoal4 = GPData4.goalsData4
+        findViewById<EditText>(R.id.typeYourGoal4).apply {
+            setText(setGoal4)
+        }
 
         //BG background
         val bgcolorsaved = BGValue.bgColourChange
@@ -50,6 +100,19 @@ class GoalsPage : ComponentActivity() {
         backToHomeButton.setOnClickListener {
             val toHomeButtonScreen = Intent(this, HomePage::class.java)
             startActivity(toHomeButtonScreen)
+        }
+
+        val savetoHomeButton = findViewById<Button>(R.id.saveButton)
+        savetoHomeButton.setOnClickListener {
+
+            GPData1.goalsData1 = goal1.text.toString()
+            GPData2.goalsData2 = goal2.text.toString()
+            GPData3.goalsData3 = goal3.text.toString()
+            GPData4.goalsData4 = goal4.text.toString()
+
+
+            val saveToHomePageScreen = Intent(this, HomePage::class.java)
+            startActivity(saveToHomePageScreen)
         }
     }
 }
